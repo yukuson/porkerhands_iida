93b85258bd4ce6bf745898d827bc7dec1ed52acc
飯田 雄介 <yiida@netprotections.co.jp>
2017-04-18 09:42:52 +0900

    エラーコードを作成中。 個数チェックと重複チェックは作成完了。 残るは、スートと数値が範囲内のものかのチェックを記載する。

--- app/controllers/porkerhands_controller.rb
+++ app/controllers/porkerhands_controller.rb
@@ -2,7 +2,17 @@ class PorkerhandsController < ApplicationController
   def index
   end
   def check
-    @result = params[:card]
+     inputs = params[:card].split(" ")
+     result = inputs.count
+     if result != 5
+       @result = '5つのカード指定文字を半角スペース区切りで入力してください。（例："S1 H3 D9 C13 S11"）'
+     elsif
+       if inputs.size == inputs.uniq.size
+         @result = "OK"
+       elsif
+         @result = "カードが重複しています。"
+       end
+     end
     render 'index'
   end
 

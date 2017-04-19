6704b133f578eba404828c997285f1c6f170f2dc
山崎 竜彗 <yryosui@netprotections.co.jp>
2017-04-19 16:39:49 +0900

    エラー判定：ソースコードを簡略化しました。

--- app/controllers/porkerhands_controller.rb
+++ app/controllers/porkerhands_controller.rb
@@ -2,46 +2,31 @@ class PorkerhandsController < ApplicationController
   def index
   end
   def check
-
-    #手札を半角スペース区切りで取得　例）D8 C8 D7 D12 S7
     hands = params[:card]
-    p hands
-
-    #一枚ずつcards配列に格納　例）["D8", "C8", "D7", "D12", "S7"]
     cards = hands.split
-    p cards
 
     #↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓エラー判定↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
+    error_input = '5つのカード指定文字を半角スペース区切りで入力してください。（例："S1 H3 D9 C13 S11"）'
+    error_duplication = "カードが重複しています。"
+    error_normalization = "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
+    error_flag = false
+
     if cards.count != 5
-      p 'カードの枚数が５に等しくない'
-      @result = '5つのカード指定文字を半角スペース区切りで入力してください。（例："S1 H3 D9 C13 S11"）'
+      @result = error_input
       render :action => "index"  and return
     elsif cards.size != cards.uniq.size
-      p 'カードの枚数（重複あり）とカードの枚数（重複なし）が等しくない'
-      @result = "カードが重複しています。"
+      @result = error_duplication
       render :action => "index"  and return
     else
-      trump = [ "S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11","S12","S13",
-                "H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","H11","H12","H13",
-                "D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","D11","D12","D13",
-                "C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13"]
-      p trump
-      error = "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
-      p error
-      errorplus = ""
-      p errorplus
-
       cards.reverse_each do |t|
-        if trump.include?(t) == false
-          p 'カードの中にトランプに含まれないものがある'
-          errorplus.insert(0 ,  "#{cards.index(t) + 1}番目の文字列が無効です。(#{t})\n ")
+        if t !~ /\A[SHDC]\d\z/ && t !~ /\A[SHDC]1[0-3]\z/
+          error_normalization.insert(0 , "#{cards.index(t)+1}番目の文字列が無効です。(#{t})" )
+          error_flag = true
         end
       end
-      if errorplus != ""
-        p 'カードは全てトランプに含まれている'
-        error.insert(0, "#{errorplus}")
-        @result = " #{error}  "
+      if error_flag
+        @result = error_normalization
         render :action => "index"  and return
       end
     end
@@ -68,7 +53,7 @@ class PorkerhandsController < ApplicationController
       end
     elsif numbers.uniq.size == 3
       if numbers[0] == numbers[2] || numbers[1] == numbers[3] || numbers[2] == numbers[4]
-        handlank = 3
+        handrank = 3
       else
         handrank = 2
       end
